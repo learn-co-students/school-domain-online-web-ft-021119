4ecd870430000792139fa3896ea6c1405c6559a1
github username <github email address>
2019-02-19 10:58:39 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
-# code here!
+# # School class is the model in the domain object model context
+#   # binding.pry
+class School
+ attr_accessor :name, :roster #, :add_student
+
+ #  def initialize(school_name)
+ #    @school_name = name
+ #    @roster = {}
+ # end
+ def initialize(school_name)
+     @school_name = school_name
+     @roster = {}
+   end
+
+   # def roster
+   #   @roster
+   # end
+
+   def add_student(student, grade)
+      if roster.key?(grade)
+        roster[grade] << student
+      elsif
+        roster[grade] = []
+        roster[grade] << student
+      end
+    end
+
+    def grade(grade)
+      roster[grade]
+    end
+
+    def sort
+      roster.each_value(&:sort!)
+    end
+end
+
+
+#
+# def add_student(name, grade)
+#     @student_name = name
+#     @grade = grade
+#     if @roster.include?(grade) == false
+#       @roster[grade] = []
+#     end
+#     @roster[grade] << name
+#   end
