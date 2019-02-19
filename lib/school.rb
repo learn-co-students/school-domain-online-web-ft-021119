77efd4b1a691a9518486bc59760eac40ea4be046
Burak Kocak <burakkocak884@ghotmail.com>
2019-02-19 16:20:30 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,54 @@
-# code here!
+require 'pry'
+
+class School
+
+attr_accessor :name, :roster
+#attr_reader :roster
+
+#@@object = "school"
+
+def initialize(name, roster = {})
+	#binding.pry
+	@name = name
+	#@roster = roster
+	@roster = Hash.new{|grades,names| grades[names] = []}
+	end
+
+
+#binding.pry
+def add_student(names,grades)
+
+
+	roster[grades]  << names
+  
+
+
+#binding.pry
+end
+def grade(grade)
+roster[grade]
+#roster[grade]
+end
+def sort
+	new_hash = {}
+roster.each do | keys, values|
+	values = values.sort
+
+new_hash[keys] = values
+	
+
+	end
+	#binding.pry
+
+	new_hash
+end
+
+
+
+#binding.pry
+
+end
+
+#names = name.add_student["Blake Johnson", 10]
+#name.roster
+#binding.pry
