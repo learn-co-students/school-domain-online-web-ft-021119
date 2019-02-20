6c1b761b172128b173285858d74873be1b8d9e7e
Vinh Do <vinhdo07@gmail.com>
2019-02-20 02:22:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  NEW = {}
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_student(name, grade)
+    @@roster[grade] << name
+  end
+end
